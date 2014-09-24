5f312b6cd734e16f116f9dfd0ad634251d0aff07
Simon Sinclair <simon@sinclair>
2014-09-24 12:37:51 +0100

    Update

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,85 @@
 // main.js
 //
 
-"use strict";
-
 (function(w, $, undefined) {
+  "use strict";
 
-	function init() {
-		console.log('DOM ready!');
-	}
+	var Stream = {
 
-	$(init);
+    config: {
+      notificationHangDurationMs: 3000
+    },
+
+    init: function() {
+      Stream.$elm          = $('#js-stream');
+      Stream.$loadMore     = $('#js-stream-load-more' , Stream.$elm);
+      Stream.$window       = $('#js-stream-window' , Stream.$elm);
+      Stream.$queue        = $('#js-stream-queue' , Stream.$elm);
+      Stream.$notification = $('#js-stream-notification' , Stream.$elm);
+
+      Stream.isNotifying = false;
+
+      Stream.bindEvts();
+    },
+
+    bindEvts: function() {
+      $(document).on('keyup', Stream.handleKeyup);
+      Stream.$loadMore.on('click', Stream.loadQueuedArticles);
+    },
+
+    handleKeyup: function(e) {
+      switch(e.keyCode) {
+        case 82:
+          Stream.reset();
+          break;
+        case 78:
+            Stream.triggerNotification();
+          break;
+        default:
+          console.log(e.keyCode);
+      }
+    },
+
+    triggerNotification: function() {
+      Stream.showNotification();
+      Stream.showLoadMoreCta();
+    },
+
+    showNotification: function() {
+      if(Stream.isNotifying) {
+        return;
+      }
+
+      var timeout;
+      Stream.$notification.addClass('stream__notification--visible');
+      Stream.isNotifying = true;
+      timeout = setTimeout(Stream.hideNotification, Stream.config.notificationHangDurationMs);
+    },
+
+    hideNotification: function() {
+      Stream.$notification.removeClass('stream__notification--visible');
+      Stream.isNotifying = false;
+    },
+
+    showLoadMoreCta: function() {
+      Stream.$loadMore.addClass('stream__load-more--visible');
+    },
+
+    hideLoadMoreCta: function() {
+      Stream.$loadMore.removeClass('stream__load-more--visible');
+    },
+
+    loadQueuedArticles: function(e) {
+      e.preventDefault();
+
+      Stream.hideLoadMoreCta();
+    },
+
+    reset: function() {
+      Stream.hideLoadMoreCta();
+    }
+
+  };
+
+	$(Stream.init);
 })(this, jQuery);
